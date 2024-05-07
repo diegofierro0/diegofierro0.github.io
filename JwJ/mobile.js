4c1e25f9b8f2b416e7fe040c9a86755351302da0
diegofierro0 <159593347+diegofierro0@users.noreply.github.com>
2024-05-06 18:52:05 -0700

    Add files via upload

---
+++ JwJ/mobile.js
@@ -0,0 +1,23 @@
+dropmenu = document.getElementsByClassName("dropdown-menu");
+dropdown = document.getElementsByClassName("nav-link");
+cos=document.getElementById("image");
+let coord = dropdown[0].getBoundingClientRect();
+
+let desplazamientoVertical = 100;
+coord.top = coord.top + desplazamientoVertical;
+
+drop = dropmenu[0];
+drop.style.position = "absolute";
+drop.style.top = coord.top + "px";
+drop.style.left = coord.left + "px";
+
+window.onscroll = function() { getSmaller(); };
+
+function getSmaller(){
+    if(window.pageYOffset>1){
+        cos.classList.add("small");
+    }
+    else{
+        cos.classList.remove("small");
+    }
+}
