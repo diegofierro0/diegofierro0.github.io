a6603c5023a8098017d607a6b62be8b9a230557e
diegofierro0 <159593347+diegofierro0@users.noreply.github.com>
2024-06-03 18:51:06 -0700

    Practica #7 03/06/24

--- parcial 3/spotify.sql
+++ parcial 3/spotify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-05-2024 a las 03:03:15
+-- Tiempo de generación: 04-06-2024 a las 03:50:20
 -- Versión del servidor: 10.1.39-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -25,6 +25,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `artistas_y_canciones`
+--
+
+CREATE TABLE `artistas_y_canciones` (
+  `id` int(11) NOT NULL,
+  `artistas_id` int(11) NOT NULL,
+  `cancion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `artstas`
 --
 
@@ -34,6 +46,38 @@ CREATE TABLE `artstas` (
   `descripcion` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `artstas`
+--
+
+INSERT INTO `artstas` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'Peso Pluma\r\n', 'cantante y compositor mexicano'),
+(2, 'Laufey', 'Cantautora y productora discográfica\r\n'),
+(3, 'Tu Coquette\r\n', 'Bolsita Dior, el morral\r\nUn billetal');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `canciones`
+--
+
+CREATE TABLE `canciones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `genero_id` int(11) NOT NULL,
+  `duracion` time NOT NULL,
+  `fecha` date NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  `foto` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `canciones`
+--
+
+INSERT INTO `canciones` (`id`, `nombre`, `genero_id`, `duracion`, `fecha`, `activo`, `foto`) VALUES
+(0, 'LA DURANGO', 4, '00:04:05', '2024-05-14', 0, '');
+
 -- --------------------------------------------------------
 
 --
@@ -42,8 +86,22 @@ CREATE TABLE `artstas` (
 
 CREATE TABLE `genero` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(150) NOT NULL
+  `nombre` varchar(150) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`id`, `nombre`) VALUES
+(2, 'pop'),
+(3, 'Rock'),
+(4, 'corridos'),
+(5, 'Bossanova'),
+(6, 'R&B'),
+(7, 'Banda'),
+(8, 'Hip Hop'),
+(9, 'JAZZ\r\n');
 
 -- --------------------------------------------------------
 
@@ -52,10 +110,49 @@ CREATE TABLE `genero` (
 --
 
 CREATE TABLE `membresia` (
-  `id` int(11) NOT NULL,
-  `descripcion` varchar(100) NOT NULL,
+  `id1` int(11) NOT NULL,
+  `descripcion` varchar(100) CHARACTER SET utf8 NOT NULL,
   `precio` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `membresia`
+--
+
+INSERT INTO `membresia` (`id1`, `descripcion`, `precio`) VALUES
+(1, 'Plan Individual', 129),
+(2, 'Plan Duo', 169),
+(3, 'Plan Familiar', 199),
+(4, 'Plan Free', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `playlist`
+--
+
+CREATE TABLE `playlist` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `duracion` time NOT NULL,
+  `total_can` int(11) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `publico` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `playlist_canciones`
+--
+
+CREATE TABLE `playlist_canciones` (
+  `id` int(11) NOT NULL,
+  `playlist_id` int(11) NOT NULL,
+  `cancion_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -75,6 +172,13 @@ CREATE TABLE `usuarios` (
 --
 
 --
+-- Indices de la tabla `artistas_y_canciones`
+--
+ALTER TABLE `artistas_y_canciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
 -- Indices de la tabla `artstas`
 --
 ALTER TABLE `artstas`
@@ -82,16 +186,35 @@ ALTER TABLE `artstas`
   ADD KEY `id` (`id`);
 
 --
+-- Indices de la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
 -- Indices de la tabla `genero`
 --
 ALTER TABLE `genero`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `membresia`
 --
 ALTER TABLE `membresia`
+  ADD PRIMARY KEY (`id1`);
+
+--
+-- Indices de la tabla `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indices de la tabla `playlist_canciones`
+--
+ALTER TABLE `playlist_canciones`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 
@@ -100,6 +223,16 @@ ALTER TABLE `membresia`
 --
 ALTER TABLE `usuarios`
   ADD KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `genero`
+--
+ALTER TABLE `genero`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
