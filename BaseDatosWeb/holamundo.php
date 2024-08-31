83e804b06f19d67ea07123e5fc2484a6bd103a88
diegofierro0 <159593347+diegofierro0@users.noreply.github.com>
2024-08-30 18:41:13 -0700

    30/08/24

---
+++ BaseDatosWeb/holamundo.php
@@ -0,0 +1,18 @@
+<?php
+
+    $nombre = "diego";
+    $numero = 4;
+    $numero2 = "4";
+
+    $total = $numero + $numero2;
+
+    echo "total = " . $total;
+
+    echo "<h1> HOLA MUNDO</h1>";
+
+    for($i=0 ; $i<10; $i++){
+        echo $i . "<br>";
+    }
+    echo "<h1 style='color: red;'> Fierro Gill Diego Guadalupe</h1>";
+
+?>
